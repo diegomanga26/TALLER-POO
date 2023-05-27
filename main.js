be84bddb666fe8beb20727e3463202d5f2b4d761
diegomanga26 <diegoalexandermanga@gmail.com>
2023-05-27 17:57:33 -0500

    feat: :sparkles: Ejercicio 3
    Ejercicio 3 del taller basico de POO

--- main.js
+++ main.js
@@ -1,71 +1,120 @@
-// EJERCICIO #2
+// EJERCICIO #3
 
 //CLASE PRINCIPAL
 
-class Animal {
-    constructor({ nombre, edad }) {
-        this.nombre = nombre;
-        this.edad = edad;
+class Figura {
+    constructor({ color, area }) {
+        this.color = color;
+        this.area = area;
     }
 
-    set setNombre(nombre) {
-        this.nombre = nombre;
+    set setColor(color) {
+        this.color = color;
     }
-    get getNombre() {
-        return this.nombre;
+    get getColor() {
+        return this.color;
     }
-    set setEdad(edad) {
-        this.edad = edad;
+    set setArea(area) {
+        this.area = area;
     }
-    get getEdad() {
-        return this.edad;
+    get getArea() {
+        return this.area;
     }
 
-    static hacerSonido(nombre, edad) {
-        return document.querySelector("#tarjeta").innerHTML = `<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-        <div class="card-header"><h5>${nombre}</h5></div>
-        <div class="card-body">
-        <p class="card-text">El animal ${nombre} de ${edad} años hizo un sonido</p>
-        </div>
-        </div>`
-    }
+    static calcularArea() {}
 }
 
-document.addEventListener("click", (e) => {
-    e.preventDefault();
-    let nombre1 = document.getElementById("nombre").value;
-    let edad1 = document.getElementById("edad").value;
-    let raza1 = document.getElementById("raza").value;
-    if (e.target.id == 'btnSonido') {
-        const animal1 = new Animal({ nombre: nombre1, edad: edad1 })
-        Animal.hacerSonido(animal1.getNombre, animal1.getEdad);
-    } else if (e.target.id == "btnMover") {
-        const perro1 = new Perro({ raza:raza1 });
-        Perro.moverCola(perro1.raza);
+document.addEventListener("change", (e) => {
+    let selectorFig = document.querySelector("#figura").value;
+    let radio = document.querySelector("#formRadio");
+    let largo = document.querySelector("#formLargo");
+    let ancho = document.querySelector("#formAncho");
+
+    radio.style.display = "none";
+    largo.style.display = "none";
+    ancho.style.display = "none";
+
+    if (selectorFig === "1") {
+        radio.style.display = "block";
+    } else if (selectorFig === "2") {
+        largo.style.display = "block";
+        ancho.style.display = "block";
     }
 });
 
-// CLASE PERRO
+document.addEventListener("click", (e) => {
+    e.preventDefault();
+    let color1 = document.getElementById("color").value;
+    let radio1 = document.getElementById("radio").value;
+    let largo1 = document.getElementById("largo").value;
+    let ancho1 = document.getElementById("ancho").value;
+    if (e.target.id === 'btnCalcular')  {
+        const circulo1 = new Circulo({ color: color1, radio: radio1 })
+        Circulo.calcularArea(circulo1.getColor,circulo1.getRadio);
+    } else if (e.target.id === 'btnCalcular') {
+        const rectangulo1 = new Rectangulo({ color: color1, largo: largo1, ancho: ancho1 })
+        Rectangulo.calcularArea(rectangulo1.getColor,rectangulo1.getLargo, rectangulo1.getAncho);
+    }
+});
 
-class Perro extends Animal {
-    constructor({nombre, edad, raza}) {
-        super({nombre, edad})
-        this.raza = raza;
+document.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+})
+
+// CLASE CIRCULO
+
+class Circulo extends Figura {
+    constructor({color, area, radio}) {
+        super({color, area})
+        this.radio = radio;
     }
-    set setRaza(raza) {
-        this.raza = raza;
+    set setRadio(radio) {
+        this.radio = radio;
     }
-    get getRaza() {
-        return this.raza;
+    get getRadio() {
+        return this.radio;
     }
 
-    static moverCola(raza) {
-        return document.querySelector("#tarjeta2").innerHTML = `<div class="card text-bg-light mb-3" style="max-width: 18rem;">
-        <div class="card-header"><h5>${raza}</h5></div>
+    static calcularArea(color, radio) {
+        return document.querySelector("#tarjeta").innerHTML=`<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+        <div class="card-header">Circulo</div>
         <div class="card-body">
-        <p class="card-text">El ${raza} está moviendo la cola.</p>
+        <p class="card-text">El circulo color ${color} tiene un area total de ${(Math.PI*(radio**2)).toFixed(3)} m^2.</p>
         </div>
-        </div>`
-    }
+        </div>
+        `;
+    };
 };
 
+// CLASE RECTANGULO
+
+class Rectangulo extends Figura {
+    constructor({color, area, largo, ancho}) {
+        super({color, area})
+        this.largo = largo;
+        this.ancho = ancho;
+    }
+    set setLargo(largo) {
+        this.largo = largo;
+    }
+    get getLargo() {
+        return this.largo;
+    }
+    set setAncho(ancho) {
+        this.ancho = ancho;
+    }
+    get getAncho() {
+        return this.ancho;
+    }
+
+    static calcularArea(color, largo, ancho) {
+        return document.querySelector("#tarjeta").innerHTML=`<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+        <div class="card-header">Rectangulo</div>
+        <div class="card-body">
+        <p class="card-text">El Rectangulo color ${color} total del rectangulo es ${(largo*ancho).toFixed(3)} m^2</p>
+        </div>
+        </div>
+        `;
+    }
+};
